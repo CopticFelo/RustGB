373e3645285a44d8b6d8691500493d2796020d6a
CopticFelo <68559765+CopticFelo@users.noreply.github.com>
2025-08-15 22:20:59 +0300

    made u8 registers public

--- src/cpu/reg_file.rs
+++ src/cpu/reg_file.rs
@@ -1,15 +1,15 @@
 #[derive(Debug)]
 struct RegFile {
-    a: u8,
-    b: u8,
-    c: u8,
-    d: u8,
-    e: u8,
-    f: u8,
-    h: u8,
-    l: u8,
+    pub a: u8,
+    pub b: u8,
+    pub c: u8,
+    pub d: u8,
+    pub e: u8,
+    pub f: u8,
+    pub h: u8,
+    pub l: u8,
     pub sp: u16,
-    pc: u16,
+    pub pc: u16,
 }
 
 impl Default for RegFile {
