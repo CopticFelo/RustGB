316d18127a0d9c7304836c3b69650e7e7e0b4df8
CopticFelo <68559765+CopticFelo@users.noreply.github.com>
2025-08-17 19:58:31 +0300

    refactor: made reg_file functions param order little endian
    for reasons that will become obvious later

--- src/cpu/reg_file.rs
+++ src/cpu/reg_file.rs
@@ -40,10 +40,10 @@ impl RegFile {
             pc: 0x100,
         }
     }
-    pub fn read_u16(hi: &u8, lo: &u8) -> u16 {
+    pub fn read_u16(lo: &u8, hi: &u8) -> u16 {
         (*hi as u16) << 8 | *lo as u16
     }
-    pub fn write_u16(hi: &mut u8, lo: &mut u8, value: u16) {
+    pub fn write_u16(lo: &mut u8, hi: &mut u8, value: u16) {
         *hi = (value >> 8) as u8;
         *lo = value as u8;
     }
