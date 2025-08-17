fe027bd6a6ad4a1af663f05b71947429d91435ca
CopticFelo <68559765+CopticFelo@users.noreply.github.com>
2025-08-17 17:57:03 +0300

    fix: fixed clock struct types mismatch
    i honestly have no clue how this works but i'll just leave at that for
    now until i hit a road block

--- src/cpu/clock.rs
+++ src/cpu/clock.rs
@@ -1,12 +1,12 @@
 #[derive(Debug)]
 struct Clock {
     m_cycles: u32,
-    c_cycles: u64,
+    t_cycles: u64,
 }
 
 impl Clock {
-    fn inc_cycle(&self, count: u8) {
-        self.m_cycles += count;
-        self.c_cycles += count * 4;
+    fn inc_cycle(&mut self, count: u8) {
+        self.m_cycles += count as u32;
+        self.t_cycles += count as u64 * 4;
     }
 }
