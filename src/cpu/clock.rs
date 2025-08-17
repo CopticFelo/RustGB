3847815c2bd83c650eb016a047f827e81fce52af
CopticFelo <68559765+CopticFelo@users.noreply.github.com>
2025-08-17 19:54:11 +0300

    refactor: made Clock struct public with only a tick method
    made more sense tbh

--- src/cpu/clock.rs
+++ src/cpu/clock.rs
@@ -1,12 +1,12 @@
-struct Clock {
 #[derive(Debug, Default)]
+pub struct Clock {
     m_cycles: u32,
     t_cycles: u64,
 }
 
 impl Clock {
-    fn inc_cycle(&mut self, count: u8) {
-        self.m_cycles += count as u32;
-        self.t_cycles += count as u64 * 4;
+    pub fn tick(&mut self) {
+        self.m_cycles += 1_u32;
+        self.t_cycles += 4_u64;
     }
 }
