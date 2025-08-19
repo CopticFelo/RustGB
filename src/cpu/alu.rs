057f996d2c1d6025d5362eff34770c35df1e8678
CopticFelo <68559765+CopticFelo@users.noreply.github.com>
2025-08-19 05:09:36 +0300

    feat(alu): added a function to read bits from a number

--- src/cpu/alu.rs
+++ src/cpu/alu.rs
@@ -5,3 +5,12 @@ pub fn write_u16(lo: &mut u8, hi: &mut u8, value: u16) {
     *hi = (value >> 8) as u8;
     *lo = value as u8;
 }
+pub fn read_bits(num: u8, index: u8, length: u8) -> u8 {
+    let mut out = 0;
+    let mut index = index;
+    for i in 0..length {
+        out += ((num >> index) & 1) * 2_u8.pow(i as u32);
+        index += 1;
+    }
+    out
+}
