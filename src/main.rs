fc2359e9db2f2433c1373bc042195b775f836d65
CopticFelo <68559765+CopticFelo@users.noreply.github.com>
2025-08-19 05:07:31 +0300

    feat: added some debug assertions
    to make my life way easier

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,22 @@ mod mem;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let mut rom_path: String = String::new();
-    if args.len() < 2 {
-        print!("Input ROM:");
-        io::stdout().flush().unwrap();
-        io::stdin()
-            .read_line(&mut rom_path)
-            .expect("Input error occoured");
-    } else {
-        rom_path = args[1].clone();
+    #[cfg(not(debug_assertions))]
+    {
+        if args.len() < 2 {
+            print!("Input ROM:");
+            io::stdout().flush().unwrap();
+            io::stdin()
+                .read_line(&mut rom_path)
+                .expect("Input error occoured");
+            rom_path = rom_path.trim().to_string();
+        } else {
+            rom_path = args[1].clone();
+        }
+    }
+    #[cfg(debug_assertions)]
+    {
+        rom_path = "/Users/felo/Emulation/GB/tetris.gb".to_string();
     }
     println!("Reading input rom: {rom_path}");
     let rom = fs::read(rom_path).expect("Failed to read file");
