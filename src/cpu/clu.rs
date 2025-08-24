2adcdae8878ab8f9db9668bdaa0b678b5f33e88f
CopticFelo <68559765+CopticFelo@users.noreply.github.com>
2025-08-24 05:34:35 +0300

    feat(clu): made functions increment the cycle

--- src/cpu/clu.rs
+++ src/cpu/clu.rs
@@ -39,6 +39,7 @@ impl<'a> CLU<'a> {
     fn load_from(&mut self, opcode: u8) -> Result<(), String> {
         let mut src = alu::read_bits(opcode, 0, 3);
         if src == 6 {
+            self.clock.tick();
             src = self
                 .memory
                 .read(alu::read_u16(&self.registers.l, &self.registers.h))?;
@@ -47,6 +48,7 @@ impl<'a> CLU<'a> {
         }
         let dst = alu::read_bits(opcode, 3, 3);
         if dst == 6 {
+            self.clock.tick();
             self.memory
                 .write(alu::read_u16(&self.registers.l, &self.registers.h), src)?;
         } else {
@@ -55,17 +57,23 @@ impl<'a> CLU<'a> {
         Ok(())
     }
 
-    fn cond_jmp(&mut self, opcode: u8, is_relative: bool) -> Result<(), String> {
+    fn jmp(&mut self, opcode: u8, is_relative: bool) -> Result<(), String> {
+        let target_address: u16;
+        let is_conditional: bool;
+        if is_relative {
+            is_conditional = opcode != 0x18;
+            target_address = (self.registers.pc as i16 + self.fetch() as i16) as u16;
+        } else {
+            is_conditional = opcode != 0xC3;
+            target_address = alu::read_u16(&self.fetch(), &self.fetch());
+        }
         if self
             .registers
             .match_condition(alu::read_bits(opcode, 3, 2))?
+            || !is_conditional
         {
-            if is_relative {
-                let offset_address = self.registers.pc as i16 + self.fetch() as i16;
-                self.registers.pc = offset_address as u16;
-            } else {
-                self.registers.pc = alu::read_u16(&self.fetch(), &self.fetch());
-            }
+            self.registers.pc = target_address;
+            self.clock.tick();
         }
         Ok(())
     }
@@ -76,11 +84,9 @@ impl<'a> CLU<'a> {
             println!("{:#X}", opcode);
             println!("{:#X}", self.registers.pc);
             match opcode {
-                0x0 => (),                                                                           // NOP
-                0xC3 | 0xD3 | 0xCA | 0xDA => self.cond_jmp(opcode, false)?, // JP cc, imm16
-                0xC4 => self.registers.pc = alu::read_u16(&self.fetch(), &self.fetch()), // JP imm16
-                0x20 | 0x30 | 0x28 | 0x38 => self.cond_jmp(opcode, true)?,  // JR cc, imm8
-                0x18 => self.registers.pc = (self.registers.pc as i16 + self.fetch() as i16) as u16, // JR imm8
+                0x0 => (),                                                                       // NOP
+                0xC2 | 0xD2 | 0xCA | 0xDA | 0xC3 => self.jmp(opcode, false)?, // JP cc, imm16 | JP imm16
+                0x20 | 0x30 | 0x28 | 0x38 | 0x18 => self.jmp(opcode, true)?, // JR cc, imm8 | JR imm8
                 0xE9 => self.registers.pc = alu::read_u16(&self.registers.l, &self.registers.h), // JP hl
                 0x40..0x80 => self.load_from(opcode)?, // LD r8, r8 | LD r8, [hl] | LD [hl], r8
                 _ => eprintln!("Unimplemented"),
