2d93712616604585c4cb096cf4938154b6c0f936
CopticFelo <68559765+CopticFelo@users.noreply.github.com>
2025-09-06 21:26:51 +0300

    style(alu): added some spacing between alu functions

--- src/cpu/alu.rs
+++ src/cpu/alu.rs
@@ -1,10 +1,12 @@
 pub fn read_u16(lo: &u8, hi: &u8) -> u16 {
     (*hi as u16) << 8 | *lo as u16
 }
+
 pub fn write_u16(lo: &mut u8, hi: &mut u8, value: u16) {
     *hi = (value >> 8) as u8;
     *lo = value as u8;
 }
+
 pub fn read_bits(num: u8, index: u8, length: u8) -> u8 {
     let mut out = 0;
     let mut index = index;
