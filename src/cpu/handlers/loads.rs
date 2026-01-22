75d897e7ad1a0c8038969cecdc32ed81635709c6
CopticFelo <68559765+CopticFelo@users.noreply.github.com>
2026-01-23 01:38:51 +0200

    feat(loads): implemeneted ld r8, imm8 instruction

--- src/cpu/handlers/loads.rs
+++ src/cpu/handlers/loads.rs
@@ -1,8 +1,8 @@
-use crate::cpu::{cpu_context::CpuContext, operands::R8};
+use crate::cpu::{alu, cpu_context::CpuContext, operands::R8};
 
 pub fn load_from(context: &mut CpuContext, opcode: u8) -> Result<(), String> {
     print!("ld ");
-    let src_param = R8::get_r8_param(false, opcode, 0, context)?;
+    let src_param = R8::get_r8_param(alu::read_bits(opcode, 6, 1) == 0, opcode, 0, context)?;
     let src = src_param.read(context)?;
     let dst_param = R8::get_r8_param(false, opcode, 3, context)?;
     dst_param.log();
