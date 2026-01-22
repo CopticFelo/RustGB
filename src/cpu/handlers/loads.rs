c2c769353ef28ddef322a71ae0b7cc95a097f0c4
CopticFelo <68559765+CopticFelo@users.noreply.github.com>
2026-01-23 01:27:40 +0200

    fix(log): ld destination not being logged

--- src/cpu/handlers/loads.rs
+++ src/cpu/handlers/loads.rs
@@ -4,8 +4,9 @@ pub fn load_from(context: &mut CpuContext, opcode: u8) -> Result<(), String> {
     print!("ld ");
     let src_param = R8::get_r8_param(false, opcode, 0, context)?;
     let src = src_param.read(context)?;
-    src_param.log();
     let dst_param = R8::get_r8_param(false, opcode, 3, context)?;
+    dst_param.log();
+    src_param.log();
     dst_param.write(context, src)?;
     Ok(())
 }
