b3f48b7489f9eb8c5fe80e8f81d0809c137f20bf
CopticFelo <68559765+CopticFelo@users.noreply.github.com>
2026-01-26 03:43:35 +0200

    feat(loads): implemented ld [r16mem] a
    loads a into r16mem (BC, DE, HL+, HL-)

--- src/cpu/handlers/loads.rs
+++ src/cpu/handlers/loads.rs
@@ -24,3 +24,19 @@ pub fn load16(context: &mut CpuContext, opcode: u8) -> Result<(), String> {
     print!("ld r16 imm16");
     Ok(())
 }
+
+pub fn load_r16mem_a(opcode: u8, context: &mut CpuContext) -> Result<(), String> {
+    let param = R16::new(opcode, 4, R16Type::R16Mem)?;
+    let addr = param.read(&context.registers);
+    context
+        .memory
+        .write(&mut context.clock, addr, context.registers.a)?;
+    let raw_param = alu::read_bits(opcode, 4, 2);
+    if raw_param == 0x2 {
+        param.write(addr + 1, &mut context.registers);
+    } else if raw_param == 0x3 {
+        param.write(addr - 1, &mut context.registers);
+    }
+    print!("ld [r16mem] a");
+    Ok(())
+}
